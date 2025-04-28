d137984739eab61f3a0840aabd0fc0cabee91a39
Mxiaobo <33818408+Mxiaobo@users.noreply.github.com>
2025-04-28 10:08:21 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='meme-calc-v4';
+const CACHE='meme-calc-v5';
 self.addEventListener('install',e=>{
  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['.','index.html','manifest.json','sw.js'])));
 });
